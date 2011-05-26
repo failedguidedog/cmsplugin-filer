13418ae78e28d8a4d7c3e95ab682b4429949d891
Philipp Bosch <hello@pb.io>
2011-05-26 13:13:39 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name = "cmsplugin-filer",
-    version = "0.8.1",
+    version = "0.8.2",
     url = 'http://github.com/stefanfoulis/cmsplugin-filer',
     license = 'BSD',
     description = "django-cms plugins for django-filer",
